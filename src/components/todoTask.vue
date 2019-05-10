f73e78bdb435b9a9e4390c309544c8f6e8f6f87e
Minh tuan <minhtuan.hust@gmail.com>
2019-05-10 15:07:20 +0700

    fix warning indent and edit theme

--- src/components/todoTask.vue
+++ src/components/todoTask.vue
@@ -1,9 +1,9 @@
 <template id="task-list">
 	<div class="wrapper">
-		<input type="text" name="todo" v-model="newTask" @keyup.enter="addTask"><button v-on:click="addTask">Add task</button>
-		<button v-on:click="removeAll">Del All Task</button>
-		<div v-for="(task, index) in tasks"  :class="{done: isChecked(task)}">
-			<input type="checkbox" name="checked" v-model="task.checked" v-on:click="check(task)"><span >{{ task.text }}</span> <button v-on:click="removeTask(index)">del</button>
+		<input type="text" name="todo" v-model="newTask" @keyup.enter="addTask"><button class="btn-button" v-on:click="addTask">Add task</button>
+		<button class="btn-button" v-on:click="removeAll">Del All Task</button>
+		<div class="task" v-for="(task, index) in tasks"  :class="{done: isChecked(task)}">
+			<div class="bg-checkbox"> <input type="checkbox" name="checked" v-model="task.checked" v-on:click="check(task)"></div><span >{{ task.text }}</span> <button class="btn-button" v-on:click="removeTask(index)">del</button>
 		</div>
 	</div>
 </template>
@@ -52,7 +52,55 @@
 </script>
 
 <style type="text/css">
-	.done{
+	.done span{
 		text-decoration: line-through;
+		vertical-align: middle;
+		color: black !important;
+	}
+	input[type="text"]{
+		width: 450px;
+		height: 50px;
+		border: 1px solid #72c5c9;
+		border-radius: 3px;
+		margin-right: 5px;
+		padding-left: 30px;
+	}
+	.btn-button{
+		height: 52px;
+		padding: 0 20px;
+		background: #72c5c9;
+		border-radius: 3px;
+		border: none;
+		text-transform: uppercase;
+		color: #fff;
+		cursor: pointer;
+	}
+	.wrapper{
+		text-align: left;
+	}
+	input[type="checkbox"]{
+		height: 20px;
+		vertical-align: middle;
+		width: 20px;
+		margin: 16px;
+	}
+	.task{
+		margin-top: 20px;
+	}
+
+	.task span{
+		background: #72c5c9;
+		padding: 18px;
+		color: #fff;
+		vertical-align: middle;
+		border-top-right-radius: 3px;
+		border-bottom-right-radius: 3px;
+	}
+	.bg-checkbox{
+		display: inline-block;
+		width: 50px;
+		height: 53px;
+		vertical-align: middle;
+		background: #72c5c9;
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-
 /* eslint-disable no-new */
 new Vue({
-	el: '#app',
-	router,
-	components: { App },
-	template: '<App/>'
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
 })
